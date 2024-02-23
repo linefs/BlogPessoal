31f0bb30dc6722f5b64172976ea4381fe7136d78
linefs <4pdublack@gmail.com>
2024-02-23 15:08:15 -0300

    BlogPessoal

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
+
+
+
+function Navbar() {
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent
+  
+
+  return (
+    <>
+     <div className='w-full bg-cyan-500 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+
+            <div className='flex gap-4'>
+            <Link to='/postagens' className='hover:underline'>Postagens</Link>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+              <div className='hover:underline'>Perfil</div>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+             
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,35 @@
+
+import homeLogo from '../../assets/blog.png'
+import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagem';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+
+
+
+function Home() {
+    return (
+        <>
+        <div className="bg-fuchsia-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Olá!</h2>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+  
+              <div className="flex justify-around gap-4">
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
+            </div>
+  
+            <div className="flex justify-center ">
+              <img src={homeLogo} alt="" className='w-2/3' />
+      
+            </div>
+          </div>
+        </div>
+        <ListaPostagens />
+      </>
+    );
+}
+
+export default Home;
